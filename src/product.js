3057aacc5a119c627e6ee8787439750baa29c580
SunhyukKim <sh.kim@specup.com>
2019-11-01 15:03:26 +0900

    test

--- src/product.js
+++ src/product.js
@@ -7,9 +7,7 @@ class ProductCategoryRow extends React.Component {
     const category = this.props.category;
     return (
       <tr>
-        <th colSpan="2">
-          {category}
-        </th>
+        <th colSpan="2">{category}</th>
       </tr>
     );
   }
@@ -18,11 +16,11 @@ class ProductCategoryRow extends React.Component {
 class ProductRow extends React.Component {
   render() {
     const product = this.props.product;
-    const name = product.stocked ?
-      product.name :
-      <span style={{color: 'red'}}>
-        {product.name}
-      </span>;
+    const name = product.stocked ? (
+      product.name
+    ) : (
+      <span style={{ color: "red" }}>{product.name}</span>
+    );
 
     return (
       <tr>
@@ -41,7 +39,7 @@ class ProductTable extends React.Component {
     const rows = [];
     let lastCategory = null;
 
-    this.props.products.forEach((product) => {
+    this.props.products.forEach(product => {
       if (product.name.indexOf(filterText) === -1) {
         return;
       }
@@ -52,15 +50,11 @@ class ProductTable extends React.Component {
         rows.push(
           <ProductCategoryRow
             category={product.category}
-            key={product.category} />
+            key={product.category}
+          />
         );
       }
-      rows.push(
-        <ProductRow
-          product={product}
-          key={product.name}
-        />
-      );
+      rows.push(<ProductRow product={product} key={product.name} />);
       lastCategory = product.category;
     });
 
@@ -79,21 +73,35 @@ class ProductTable extends React.Component {
 }
 
 class SearchBar extends React.Component {
-  render() {
-    const filterText = this.props.filterText;
-    const inStockOnly = this.props.inStockOnly;
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
 
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked);
+  }
+
+  render() {
     return (
       <form>
         <input
           type="text"
           placeholder="Search..."
-          value={filterText} />
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
         <p>
           <input
             type="checkbox"
-            checked={inStockOnly} />
-          {' '}
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
+          />{" "}
           Only show products in stock
         </p>
       </form>
@@ -105,9 +113,24 @@ class FilterableProductTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      filterText: '',
+      filterText: "",
       inStockOnly: false
     };
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+
+  handleInStockChange(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    });
   }
 
   render() {
@@ -116,6 +139,8 @@ class FilterableProductTable extends React.Component {
         <SearchBar
           filterText={this.state.filterText}
           inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
         />
         <ProductTable
           products={this.props.products}
@@ -127,14 +152,38 @@ class FilterableProductTable extends React.Component {
   }
 }
 
-
 const PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball"
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch"
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5"
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
 ];
 
 function ProductApp() {
@@ -144,4 +193,7 @@ function ProductApp() {
   );
 }
 
+function tmp(){
+  return 1;
+}
 export default ProductApp;
