df9773f165fc3a33978807f53a61e2e405564afa
SunhyukKim <sh.kim@specup.com>
2019-11-01 14:59:07 +0900

    step4

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import ProductApp from './product';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ProductApp />, document.getElementById('root'));
